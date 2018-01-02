caf157ad517eea033212e885428a795893527ae9
Angelo Ribeiro <angelomr@gmail.com>
2018-01-02 21:50:03 -0200

    Criação do projeto Thread

---
+++ ThreadProj/ThreadProj/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // criando uma thread secundaria
+            Thread t = new Thread(ThreadB);
+            // iniciando a thread
+            t.Start();
+
+            for (int a = 0; a < 100; a++)
+            {
+                Console.Write("A");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        // thread secundaria
+        static void ThreadB()
+        {
+            for (int x = 0; x < 100; x++)
+            {
+                Console.Write("B");
+            }
+        }
+    }
+}
